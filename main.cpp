1999d1954aa47bba8ffd7e8e5b6d6a11041cbc6c
Matheus Victor <matheusvictor.salles@gmail.com>
2022-05-29 21:26:43 -0300

    Modifica testes de chamadas no main

--- main.cpp
+++ main.cpp
@@ -1,33 +1,20 @@
+#include <stack>
+#include <time.h>
 #include <vector>
+#include <stdlib.h>
+#include <iostream>
 
 #include "ACE.h"
-
-#define QUANTIDADE_CARTAS 13
+#include "Simulador.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
     
-    vector<char> naipes;
-    naipes.push_back('a');
-    naipes.push_back('b');
-    naipes.push_back('c');
-    naipes.push_back('d');
-
-    vector<Carta> deck;
-
-    ACE aceDeck;
-    
-    for (int i = 0; i < naipes.size(); i ++) {
-        for(int j = 0; j < QUANTIDADE_CARTAS; j ++) {
-            Carta carta(naipes[i], j);
-            aceDeck.addCarta(carta);
-        }        
-    }
-
-    while(!aceDeck.isDeckVazio()) {
-        cout << aceDeck.pegarCarta()->obterNaipe() << endl;
-    }
+    Simulador* simulador = new Simulador(2);
+    simulador->iniciarBaralho();
+    Carta* mico = simulador->retirarMico();
+    mico->obterDetalhesCarta();
 
     return 0;
 }
