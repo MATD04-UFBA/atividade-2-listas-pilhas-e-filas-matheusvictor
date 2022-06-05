2160a380698472bcd854bdcdde6684827d916822
Matheus Victor <matheusvictor.salles@gmail.com>
2022-06-05 17:14:26 -0300

    Refatora classe Simulador

--- include/Simulador.h
+++ include/Simulador.h
@@ -1,30 +1,49 @@
-#include "Jogador.h"
+#include "ListaJogadores.h"
 #include "Carta.h"
 #include "ACE.h"
 
 #define MIN_JOGADORES 2
 #define MAX_JOGADORES 4
 
+using namespace std;
+
 class Simulador {
+
+public:
+    Simulador(int quantidadeJogadores);
+    ~Simulador();
+
+    void iniciarJogo();
 
 private:
     int contadorRodadas;
-    //Jogador jogadores[];
+    int quantidadeJogadores;
     ACE* baralho;
     Carta* mico;
+    ListaJogadores* jogadores;
 
-public:
-    Simulador(/* args */);
-    Simulador(int quantidadeJogadores);
-    ~Simulador();
-    
-    void iniciarBaralho();
+     void iniciarBaralho();
+
+    void imprimirBaralho();
+
+    void imprimirTamanhoBaralho();
 
     void iniciarJogadores();
+
+    void imprimirJogadores();
+
+    void distribuirCartas();
 
     void iniciarPartida();
 
     Carta* retirarMico();
+
+    void imprimirMico();
+
+    ListaJogadores* obterJogadores();
     
+    void exibirMaoJogador(Jogador* j);
+
     bool verificarCartasJogadores();
+   
 };
--- src/Simulador.cpp
+++ src/Simulador.cpp
@@ -1,20 +1,62 @@
 #include "Simulador.h"
 #include "Carta.h"
 
-Simulador::Simulador() { }
 
-Simulador::Simulador(int qtdJogadores) { 
-    baralho = new ACE;
+Simulador::Simulador(int quantidadeJogadores) { 
+    this->baralho = new ACE;
+    this->mico = new Carta;
+    this->jogadores = new ListaJogadores;
+    this->contadorRodadas = 0;
+    this->quantidadeJogadores = quantidadeJogadores;
 }
 
 Simulador::~Simulador() { }
 
-void Simulador::iniciarBaralho() {
+void Simulador::iniciarJogo() {    
     this->baralho->inicializarACE();
-    this->baralho->embaralhar();
+    
+    // cout << "Baralho inicializado! Deseja Deseja imprimi-lo antes de embaralha-lo? = ";
+    // this->baralho->imprimir();
+
+    this->baralho->embaralhar();    
+    // cout << "O Mico foi retirado! Deseja Deseja imprimir o Mico [S/N] = ";
+    Carta* mico = this->retirarMico();
+    // mico->obterDetalhesCarta();
+
+    this->iniciarJogadores();
+    this->distribuirCartas();
+
 }
 
-void Simulador::iniciarJogadores() { }
+void Simulador::iniciarJogadores() {
+
+    if (this->quantidadeJogadores < 2) {
+        std::cout << "Valor invalido! Assumindo o valor minimo de 2 jogadores..." << std::endl;
+        this->quantidadeJogadores = MIN_JOGADORES;
+    } else if (this->quantidadeJogadores > 4) {
+        std::cout << "Valor invalido! Assumindo o valor maximo de 4 jogadores..." << std::endl;
+        this->quantidadeJogadores = MAX_JOGADORES;
+    }
+
+    for (int i = 0; i < this->quantidadeJogadores; i++) {
+        int numero = this->jogadores->tamanho() + 1;
+        Jogador* j = new Jogador("Player " + std::to_string(numero));
+        this->jogadores->inserirJogador(j);
+    }
+
+}
+
+void Simulador::distribuirCartas() {
+    
+    No* no = this->jogadores->obterInicioLista();
+
+    while (!this->baralho->isDeckVazio()) {        
+        Carta* c = this->baralho->pegarCarta();
+        no->obterDado()->addCartaMao(c);
+        no = no->obterProximo();        
+    }
+
+}
 
 void Simulador::iniciarPartida() { 
     while (verificarCartasJogadores()) {
@@ -24,6 +66,14 @@ void Simulador::iniciarPartida() {
 
 Carta* Simulador::retirarMico() {
     return this->baralho->pegarCarta();
+}
+
+void Simulador::exibirMaoJogador(Jogador* j) {
+    j->exibirNumeroCartas();
+}
+
+ListaJogadores* Simulador::obterJogadores() {
+    return this->jogadores;
 }
 
 bool Simulador::verificarCartasJogadores() {
